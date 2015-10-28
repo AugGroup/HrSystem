3f2117959230f51b4d2d03d688b955349db845a7
bill <bisbifltl@gmail.com>
2015-10-28 17:17:26 +0700

    add reservationDto

--- src/main/java/com/aug/hr/controller/ReservationController.java
+++ src/main/java/com/aug/hr/controller/ReservationController.java
@@ -7,6 +7,7 @@
 package com.aug.hr.controller;
 
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.aug.hrdb.dto.ReservationDto;
 import com.aug.hrdb.entities.Reservation;
+import com.aug.hrdb.services.ReservationService;
+import com.google.common.collect.Lists;
 
 
 @Controller
 public class ReservationController {
 
+	@Autowired ReservationService reservationService;
+	
 	
 	@RequestMapping(value="/reservation",method=RequestMethod.GET)
 	public String reservationPage(){
@@ -29,12 +35,25 @@ public class ReservationController {
 	}
 	
 	@RequestMapping(value="/reservation/ajax/getAllReservation",method=RequestMethod.GET)
-	public @ResponseBody List<Reservation> findAppointment(@RequestParam(value="start") String start,
+	public @ResponseBody List<ReservationDto> findReservation(@RequestParam(value="start") String start,
 			@RequestParam(value="end") String end, @RequestParam(value="_",required = false) String underscore, 
-			@RequestParam(value="timezone",required = false) String timezone, @RequestParam Integer mailStatus) throws ParseException {
+			@RequestParam(value="timezone",required = false) String timezone) throws ParseException {
 			
+		    System.out.println("start : "+start+"   end : "+end);
 		
-			//List<Reservation> reservation ;
-		return null;
+		    
+			List<ReservationDto> reservations = new ArrayList<ReservationDto>();
+			reservations = reservationService.findByDateRange(start, end);
+	
+/*		    Reservation reservation = new Reservation();
+		    reservation.setId(1);
+		    reservation.setName("test");
+			List<Reservation> reservations = Lists.newArrayList(reservation);*/
+		    
+		if (reservations.size()==0) {
+			return null;
+		}else {
+			return reservations;
+		}
 	}
 }
--- src/main/webapp/js/reservation.js
+++ src/main/webapp/js/reservation.js
@@ -3,7 +3,7 @@
  */
 
 /* ------------------------------ Form Calendar ------------------------*/
-var $calendar = $("#calendar");
+var $calendar ;//= $("#calendar");
 function renderCalendar(){
 	$calendar = $("#calendar").fullCalendar({
 		header:{
@@ -11,7 +11,8 @@ function renderCalendar(){
 			center: "title",
 			right: "month,agendaWeek,agendaDay"
 		},
-		
+		selectable : true,
+		editable: true,
 		defaultDate: moment(),
 		select: function(start,end){
 			
@@ -19,9 +20,12 @@ function renderCalendar(){
 		eventClick: function(event,element){
 			
 		},
-		lang : 'th',
-		events:[
-			{
+		eventLimit: true,
+		//lang : 'th',
+		timezone: "Asia/Bangkok",
+		ignoreTimezone:false,
+		eventSources:[
+			/*{
 				title: 'All Day Event',
 				start: '2015-10-27'
 			},
@@ -29,7 +33,21 @@ function renderCalendar(){
 				title: 'Long Event',
 				start: '2015-10-26',
 				end: '2015-10-27'
-			}
+			},*/
+			{
+				url : 'reservation/ajax/getAllReservation',
+				type: 'GET',
+				success: function(data) {
+					/*console.log("load: ");
+					console.log(data);*/
+					//alert("success");
+					console.log(data);
+				},
+				error: function(error) {
+					alert("ERROR");
+					console.log(error);
+				}
+		    }
 		]
 			
 		
